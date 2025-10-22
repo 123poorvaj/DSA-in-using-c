2500fc4f6086a5974992aab0fa5d6fe2947f09be
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-10-22 18:54:54 +0530

    Add files via upload

--- menudreven.c
+++ menudreven.c
@@ -37,13 +37,34 @@ void linearsearch(int key ,int n ,int *arr){
      printf("key is not found\n");
 
 }
+void binarysearch(int *arr,int key,int low,int height)
+{   int found=0;
+    while(low<=height){
+        int mid=(low+height)/2;
+        if(arr[mid]==key){
+            printf("key is prasent at the index %d\n",mid);
+            found=1;
+            break;
+        }
+        else if(arr[mid]>key){
+            height=mid-1;
+        }
+        else if(arr[mid]<key){
+            low=mid+1;
+        }
+    }
+    if(found!=1)
+    printf("key is not found\n");
+
+    
+}
 
 int main(){
     int choice,n=0,key;
     int arr[32];
     while(1)
     {
-        printf("1.input\n2.output\n3.linear search\n4.exit\n");
+        printf("1.input\n2.output\n3.linear search\n4.binar ysearch \n5.exit\n");
         printf("Enter your choice\n");
         scanf("%d",&choice);
         switch (choice)
@@ -63,10 +84,17 @@ int main(){
                     printf("enter the key value\n");
                     scanf("%d",&key);
                     linearsearch(key,n,arr);
-
+                    break;
                 }
-
             case 4:
+            {
+                printf("enter the key to search\n");
+                scanf("%d",&key);
+                binarysearch(arr,key,0,n-1);
+                break;
+
+            }
+            case 5:
                 exit(0);
             
             default: 
